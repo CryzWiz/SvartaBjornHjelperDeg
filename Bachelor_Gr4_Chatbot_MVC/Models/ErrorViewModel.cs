fd27d10a6343e3d928d71175ac9aff9005b26c5f
Allan Arnesen <allan.arnesen@gmail.com>
2018-02-20 10:48:59 +0100

    Add project files.

---
+++ Bachelor_Gr4_Chatbot_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bachelor_Gr4_Chatbot_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
