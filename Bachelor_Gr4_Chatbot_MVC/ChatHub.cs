82c93d987121a91d2b4631f224f1271651e96f4b
Benedicte Karlsen <bka049@post.uit.no>
2018-03-01 03:25:43 +0100

    3.5 - Only testing with proxy, not done at all..

--- Bachelor_Gr4_Chatbot_MVC/ChatHub.cs
+++ Bachelor_Gr4_Chatbot_MVC/ChatHub.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace Bachelor_Gr4_Chatbot_MVC
 {
+    /// Referanser: 
+    /// https://code.msdn.microsoft.com/ASPNET-CORE-20-uses-7a771742
+    /// https://docs.microsoft.com/en-us/aspnet/signalr/overview/guide-to-the-api/mapping-users-to-connections
     public class ChatHub : Hub
     {
         /*public Task Send(string message)
@@ -13,9 +16,52 @@ namespace Bachelor_Gr4_Chatbot_MVC
             return Clients.All.InvokeAsync("Send", message);
         }*/
 
+        /// <summary>
+        /// Send a message to everyone. 
+        /// </summary>
+        /// <param name="name">Name of the author of the message</param>
+        /// <param name="message">Message content</param>
+        /// <returns></returns>
         public Task Send(string name, string message)
         {
             return Clients.All.InvokeAsync("broadcastMessage", name, message);
+        }
+
+        /// <summary>
+        /// Send a message to a specified user group. 
+        /// </summary>
+        /// <param name="groupName"></param>
+        /// <param name="message"></param>
+        public void SendChatMessage(string groupName, string message)
+        {
+            string name = Context.User.Identity.Name;
+            Clients.Group(groupName).InvokeAsync("SendMessage", name, message);
+        }
+
+        /*public override Task OnConnectedAsync()
+        {
+            string name = Context.User.Identity.Name;
+            Groups.AddAsync(Context.ConnectionId, name);
+            return base.OnConnectedAsync();
+        }*/
+
+ 
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+
+        /// <summary>
+        /// Connected user leaves the specified group. 
+        /// </summary>
+        /// <param name="groupName">Group name</param>
+        /// <returns></returns>
+        public async Task LeaveGroup(string groupName)
+        {
+            await Groups.RemoveAsync(Context.ConnectionId, groupName);
+
         }
     }
 }
--- Bachelor_Gr4_Chatbot_MVC/wwwroot/js/site.js
+++ Bachelor_Gr4_Chatbot_MVC/wwwroot/js/site.js
@@ -1,1 +1,17 @@
-﻿// Write your JavaScript code.
+﻿// Referanser: 
+// https://docs.microsoft.com/en-us/aspnet/signalr/overview/guide-to-the-api/hubs-api-guide-javascript-client
+
+function testWithProxy() {
+    var chatHubProxy = $.connection.chatHub;
+    chatHubProxy.client.Send = function (name, message) {
+        console.log(name + ' ' + message);
+    }
+
+    $.connection.hub.start().done(function () {
+        $('#testProxy').click(function () {
+            var msg = $('#messageProxy').val('');
+            chatHubProxy.server.Send('testNavn', msg);
+            msg.focus();
+        });
+    });
+}
