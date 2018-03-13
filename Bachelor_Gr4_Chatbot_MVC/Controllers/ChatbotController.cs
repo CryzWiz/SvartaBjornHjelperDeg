181777b50f94dcd0a7abd20df939690a3df7445b
Allan Arnesen <allan.arnesen@gmail.com>
2018-03-13 15:51:42 +0100

    .

--- Bachelor_Gr4_Chatbot_MVC/Controllers/ChatbotController.cs
+++ Bachelor_Gr4_Chatbot_MVC/Controllers/ChatbotController.cs
@@ -5,13 +5,17 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Connector.DirectLine;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Bachelor_Gr4_Chatbot_MVC.Controllers
 {
+    
     public class Chat
     {
         public string Text { get; set; }
@@ -29,11 +33,28 @@ namespace Bachelor_Gr4_Chatbot_MVC.Controllers
         private HttpResponseMessage response2;
         private HttpResponseMessage response3;
         private HttpClient client;
-        private static string directLineAddress = "https://directline.botframework.com/v3/directline/conversations";
-        private static string BaseAddress = "https://chatbotsvartabjorn.azurewebsites.net/";
+
+        private static string contentType = "application/json";
+        private static string BotSecret = "SGOlKUQmphg.cwA.0ho.CYEuXR9VGPjZ19d7n7zKKjSYpVamhVYgh7qEdE_gxn0";
+
+        private static string directLineAddress_V3 = "https://directline.botframework.com/v3/directline/conversations";
+        private static string tokenAddress_V3 = "/v3/directline/tokens/generate";
+        private static string botAutorizeTokenScheme_V3 = "Bearer";
+        private static string directLineConversation_V3 = "/v3/directline/conversations/";
+
+        private static string directLineAddress_V1 = "https://directline.botframework.com/api/conversations";
+        private static string tokenAddress_V1 = "/api/tokens/conversation";
+        private static string botAutorizeTokenScheme_V1 = "BotConnector";
+        private static string directLineConversation_V1 = "/api/conversations";
+
         private static string ApiExt = "api/messages";
+        private static string ApiEndPoint = "https://chatbotsvartabjorn.azurewebsites.net/";
+
+
         private static string DiretlineUrl = @"https://chatbotsvartabjorn.azurewebsites.net/api/messages";
         private static string directLineSecret = "SGOlKUQmphg.cwA.0ho.CYEuXR9VGPjZ19d7n7zKKjSYpVamhVYgh7qEdE_gxn0";
+        String messagereturn;
+        Activity activity;
         private static string botId = "Chatbot_SvartaBjorn_Azure";
 
         /// <summary>
@@ -52,133 +73,88 @@ namespace Bachelor_Gr4_Chatbot_MVC.Controllers
         [HttpPost]
         public virtual async Task<IActionResult> Post(string message)
         {
-
-            bool IsReplyReceived = false;
-
+            // Create the connection using the secret token
             client = new HttpClient();
-            client.BaseAddress = new Uri("https://directline.botframework.com/v3/directline");
+            client.BaseAddress = new Uri(directLineAddress_V3);
             client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "SGOlKUQmphg.cwA.0ho.CYEuXR9VGPjZ19d7n7zKKjSYpVamhVYgh7qEdE_gxn0");
-            
-            response = await client.PostAsync("/v3/directline/tokens/generate", null);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(contentType));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(botAutorizeTokenScheme_V3, BotSecret);
+            // Fetch a new token for just this chat
+            response = await client.PostAsync(tokenAddress_V3, null);
+            Conversation conversationinfo = response.Content.ReadAsAsync(typeof(Conversation)).Result as Conversation;
+            // Clear the headers and set the new token
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(contentType));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(botAutorizeTokenScheme_V3, conversationinfo.Token);
+            // Start the conversation
+            response = await client.PostAsync(directLineConversation_V3, null);
+            Conversation currentConversation = response.Content.ReadAsAsync(typeof(Conversation)).Result as Conversation;
+            // Set the conversation url
+            string conversationUrl = directLineConversation_V3 + conversationinfo.ConversationId + "/activities";
+            // Create activity
+            Activity thisActivity = new Activity { Type = "message", Text = "tester tester", From = new ChannelAccount { Id = currentConversation.ConversationId }};
+            var myContent = JsonConvert.SerializeObject(thisActivity);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+            var byteContent = new ByteArrayContent(buffer);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            // Post the activity
+            response = await client.PostAsync(conversationUrl, byteContent);
 
-            if (response.IsSuccessStatusCode)
+            // Fetch messages
+            response = await client.GetAsync(conversationUrl);
+
+            //response = await client.PostAsJsonAsync(conversationUrl, requestMessage);
+
+
+            if (response.IsSuccessStatusCode) // Yey -> We got a connection and a reply
             {
-                var conversation = new Conversation();
-                response = await client.PostAsJsonAsync("/v3/directline/conversations", conversation);
-                //if (response.IsSuccessStatusCode)
-                //{
-                //    Conversation ConversationInfo = response.Content.ReadAsAsync(typeof(Conversation)).Result as Conversation;
-                //    string conversationUrl = ConversationInfo.ConversationId + "/messages/";
-                //    Message msg = new Message() { text = message };
-                //    response = await client.PostAsJsonAsync(conversationUrl, msg);
-                //    if (response.IsSuccessStatusCode)
-                //    {
-                //        response = await client.GetAsync(conversationUrl);
-                //        if (response.IsSuccessStatusCode)
-                //        {
-                //            MessageSet BotMessage = response.Content.ReadAsAsync(typeof(MessageSet)).Result as MessageSet;
-                //            ViewBag.Messages = BotMessage;
-                //            IsReplyReceived = true;
-                //        }
-                //    }
-                //}
-
-            }
-            return Json(response.Content.ReadAsStringAsync());
-        }
-
-
-
-
-
-        // POST api/values
-        //[HttpPost]
-        //public virtual async Task<IActionResult> Post(Activity activity)
-        //{
-        //    Activity a = activity as Activity;
-
-        //    var result = new { test = "failed -> Vi kom ikke inn i noen" };
-
-        //    if (activity.Type == ActivityTypes.Message)
-        //    {
-        //        // calculate something for us to return
-        //        int length = (activity.Text ?? string.Empty).Length;
-        //        await ReplyMessage(activity, $"You sent {activity.Text} which was {length} characters");
-        //        result = new { test = "suksess - vi har kontakt og er i ReplyMessage!" };
-        //    }
-        //    else
-        //    {
-        //        await HandleSystemMessage(activity);
-        //        if (activity is Activity)
-        //        {
-        //            var t = "Do I work?";
-        //            result = new { test = "suksess - vi har kontakt og er i HandleSystemMessage! Vi har en Activity. " +
-        //                "Activity.Type == " + a.Text};
-        //        }
-        //        else
-        //        {
-        //            result = new { test = "suksess - vi har kontakt og er i HandleSystemMessage! Activity == null" };
-        //        }
                 
-        //    }
+                //Conversation conversationinfo = response.Content.ReadAsAsync(typeof(Conversation)).Result as Conversation;
+
+                
+                //string conversationUrl = "/v3/directline/conversations/" + conversationinfo.ConversationId + "/activities";
+
+                //ChannelAccount user = new ChannelAccount { Id = conversationinfo.ConversationId, Name = "allan_name" };
+                //ConversationAccount conversationAccount = new ConversationAccount { Id = conversationinfo.ConversationId };
+                //activity = new Activity { From = user, Text = "Hei", Type = "message", Conversation = conversationAccount };
+                //Message msg = new Message() { text = message, from = user, conversationId = conversationinfo.ConversationId,
+                //type = "message"};
+                //var a = Activity.CreateMessageActivity();
+                //a.From = user;
+                //a.Text = "hello";
+                //a.Type = "message";
+                //HttpRequestMessage requestMessage = new HttpRequestMessage();
+                //requestMessage.Content = new StringContent("{\"type\":\"message\",\"from\": {\"id\": \"user1\" }, \"text\":\"Hello\"}", Encoding.UTF8, "application/json");
+                //response = await client.PostAsJsonAsync(conversationUrl, requestMessage);
+                //response = await client.GetAsync(conversationUrl);
+            }
             
-        //    //return Ok();
-        //    return Json(result);
-        //}
 
-        /// <summary>
-        /// Handles the system message.
-        /// </summary>
-        /// <param name="activity">The activity.</param>
-        //private async Task<Activity> HandleSystemMessage(Activity activity)
-        //{
-        //    switch (activity.Type)
-        //    {
-        //        case ActivityTypes.DeleteUserData:
-        //            // Implement user deletion here
-        //            // If we handle user deletion, return a real message
-        //            break;
 
-        //        case ActivityTypes.ConversationUpdate:
-        //            // Handle conversation state changes, like members being added and removed
-        //            // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-        //            // Not available in all channels
-        //            break;
-
-        //        case ActivityTypes.ContactRelationUpdate:
-        //            // Handle add/remove from contact lists
-        //            // Activity.From + Activity.Action represent what happened
-        //            break;
-
-        //        case ActivityTypes.Typing:
-        //            // Handle knowing that the user is typing
-        //            break;
-
-        //        case ActivityTypes.Ping:
-        //            await ReplyMessage(activity, "Pong");
-        //            break;
-
-        //        default:
-        //            break;
-        //    }
-
-        //    return null;
-        //}
-
-        ///// <summary>
-        ///// Replies the message.
-        ///// </summary>
-        ///// <param name="activity">The activity.</param>
-        ///// <param name="message">The message.</param>
-        //private async Task ReplyMessage(Activity activity, string message)
-        //{
-        //    var serviceEndpointUri = new Uri(activity.ServiceUrl);
-        //    var connector = new ConnectorClient(serviceEndpointUri, appCredentials);
-        //    var reply = activity.CreateReply(message);
-
-        //    await connector.Conversations.ReplyToActivityAsync(reply);
-        //}
+            
+            //if (response.IsSuccessStatusCode)
+            //{
+            //    var conversation = new Conversation();
+            //    response = await client.PostAsJsonAsync("/v3/directline/conversations", conversation);
+            //    if (response.IsSuccessStatusCode)
+            //    {
+            //        Conversation ConversationInfo = response.Content.ReadAsAsync(typeof(Conversation)).Result as Conversation;
+            //        string conversationUrl = "/v3/directline/conversations/"+ConversationInfo.ConversationId + " / activities/";
+            //        Message msg = new Message() { text = message };
+            //        response = await client.PostAsJsonAsync(conversationUrl, msg);
+            //        if (response.IsSuccessStatusCode)
+            //        {
+            //            response = await client.GetAsync(conversationUrl);
+            //            if (response.IsSuccessStatusCode)
+            //            {
+            //                MessageSet BotMessage = response.Content.ReadAsAsync(typeof(MessageSet)).Result as MessageSet;
+            //                ViewBag.Messages = BotMessage;
+            //                IsReplyReceived = true;
+            //            }
+            //        }
+            //    }
+            //}
+            return Json(response.Content.ReadAsStringAsync().Result);
+        }
     }
 }
--- Bachelor_Gr4_Chatbot_MVC/Models/BotModels/Message.cs
+++ Bachelor_Gr4_Chatbot_MVC/Models/BotModels/Message.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Bot.Connector.DirectLine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,11 +11,12 @@ namespace Bachelor_Gr4_Chatbot_MVC.Models.BotModels
         public string id { get; set; }
         public string conversationId { get; set; }
         public DateTime created { get; set; }
-        public string from { get; set; }
+        public ChannelAccount from { get; set; }
         public string text { get; set; }
         public string channelData { get; set; }
         public string[] images { get; set; }
         public Attachment[] attachments { get; set; }
         public string eTag { get; set; }
+        public string type { get;set; }
     }
 }
