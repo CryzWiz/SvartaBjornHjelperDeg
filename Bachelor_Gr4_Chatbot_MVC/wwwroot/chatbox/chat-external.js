25eff0bfd24920dd24bc596f52425889a8de5125
Benedicte Karlsen <bka049@post.uit.no>
2018-03-29 20:41:16 +0200

    Connect to SignalR only after pushing button

--- Bachelor_Gr4_Chatbot_MVC/wwwroot/chatbox/chat-external.js
+++ Bachelor_Gr4_Chatbot_MVC/wwwroot/chatbox/chat-external.js
@@ -92,97 +92,101 @@ document.addEventListener('DOMContentLoaded', function () {
     // Set initial focus to message input box.
     messageInput.focus();
 
-    // Start the connection.
-    startConnection('/chathub', function (connection) {
-        // Create a function that the hub can call to broadcast messages.
-        connection.on('broadcastMessage', function (message) {
-            // TODO: Html encode display name and message.
-            //var encodedName = name;
-            var encodedMsg = message;
-            // Add the message to the page.
-            var liElement = document.createElement('div');
-            liElement.className += "chatbox__body__message";
-            liElement.className += " chatbox__body__message--left";
-            liElement.innerHTML += '<img src="../images/narvik_kommune_small.jpg"/>';
-            liElement.innerHTML += '<p>' + encodedMsg + '</p>';
-            document.getElementById('chatbox__body').appendChild(liElement);
-            document.getElementById('chatbox__body').scrollTop = document.getElementById('chatbox__body').scrollHeight;
+    $("#connectToSignalR").click(function () {
+        // Start the connection.
+        startConnection('/chathub', function (connection) {
+            // Create a function that the hub can call to broadcast messages.
+            connection.on('broadcastMessage', function (message) {
+                // TODO: Html encode display name and message.
+                //var encodedName = name;
+                var encodedMsg = message;
+                // Add the message to the page.
+                var liElement = document.createElement('div');
+                liElement.className += "chatbox__body__message";
+                liElement.className += " chatbox__body__message--left";
+                liElement.innerHTML += '<img src="../images/narvik_kommune_small.jpg"/>';
+                liElement.innerHTML += '<p>' + encodedMsg + '</p>';
+                document.getElementById('chatbox__body').appendChild(liElement);
+                document.getElementById('chatbox__body').scrollTop = document.getElementById('chatbox__body').scrollHeight;
 
-        });
-
-
-        /// SignalR Client methods called from hub:
-        /*connection.on('send', function (message, from) {
-            groupId = from;
-            
-            displayReceivedMessage(message);
-            // TODO: Html encode message.
-            var encodedMsg = message;
-            // Add the sent message to the page.
-            var liElement = document.createElement('div');
-            liElement.className += "chatbox__body__message";
-            liElement.className += " chatbox__body__message--left";
-            liElement.innerHTML += '<img src="../images/narvik_kommune_small.jpg"/>';
-            liElement.innerHTML += '<p>' + encodedMsg + '</p>';
-            document.getElementById('chatbox__body').appendChild(liElement);
-            document.getElementById('chatbox__body').scrollTop = document.getElementById('chatbox__body').scrollHeight;
-            groupId = from;
-        });*/
-
-        connection.on('receiveMessage', function(groupFrom, message) {
-            groupId = groupFrom;
-            displayReceivedMessage(message);
-        });
-
-        connection.on('sendMessage', function (message) {
-            displaySentMessage(message);
-        });
-
-
-        
-
-
-        // TODO: for 
-        connection.on('displayQueueNumber', function (queNumber) {
-            displayReceivedMessage('Du er nå lagt i kø, en medarbeider vil svare deg så raskt som mulig. Din plass i køen er: ' + queNumber);
-        });
-
-
-    })
-        .then(function (connection) {
-            console.log('connection started'); // TODO:
-
-            // Send message
-            $("#sendmessage").click(function (event) {
-                // TODO: Bytt ut kode her
-                //connection.invoke('send', messageInput.value);
-                if (talkWithChatBot) {
-                    connection.invoke('sendToGroup', groupId, messageInput.value);
-                    //connection.invoke('sendToGroup2', groupId, talkWithChatBot, messageInput.value);
-                } else {
-                    connection.invoke('sendToGroup', groupId, messageInput.value);
-                }
-
-
-
-                // Clear text box and reset focus for next comment.
-                messageInput.value = '';
-                messageInput.focus();
-                event.preventDefault();
             });
-            
-            // Start chat by joining queue
-            $("#chatbox_placeholder").on('click', "button[id='startChat']", function (event) {
-                connection.invoke('joinQueue');
+
+
+            /// SignalR Client methods called from hub:
+            /*connection.on('send', function (message, from) {
+                groupId = from;
+                
+                displayReceivedMessage(message);
+                // TODO: Html encode message.
+                var encodedMsg = message;
+                // Add the sent message to the page.
+                var liElement = document.createElement('div');
+                liElement.className += "chatbox__body__message";
+                liElement.className += " chatbox__body__message--left";
+                liElement.innerHTML += '<img src="../images/narvik_kommune_small.jpg"/>';
+                liElement.innerHTML += '<p>' + encodedMsg + '</p>';
+                document.getElementById('chatbox__body').appendChild(liElement);
+                document.getElementById('chatbox__body').scrollTop = document.getElementById('chatbox__body').scrollHeight;
+                groupId = from;
+            });*/
+
+            connection.on('receiveMessage', function (groupFrom, message) {
+                groupId = groupFrom;
+                displayReceivedMessage(message);
+            });
+
+            connection.on('sendMessage', function (message) {
+                displaySentMessage(message);
             });
 
 
 
+
+
+            // TODO: for 
+            connection.on('displayQueueNumber', function (queNumber) {
+                displayReceivedMessage('Du er nå lagt i kø, en medarbeider vil svare deg så raskt som mulig. Din plass i køen er: ' + queNumber);
+            });
 
 
         })
-        .catch(error => {
-            console.error(error.message);
-        });
+            .then(function (connection) {
+                console.log('connection started'); // TODO:
+
+                // Send message
+                $("#sendmessage").click(function (event) {
+                    // TODO: Bytt ut kode her
+                    //connection.invoke('send', messageInput.value);
+                    if (talkWithChatBot) {
+                        connection.invoke('sendToGroup', groupId, messageInput.value);
+                        //connection.invoke('sendToGroup2', groupId, talkWithChatBot, messageInput.value);
+                    } else {
+                        connection.invoke('sendToGroup', groupId, messageInput.value);
+                    }
+
+
+
+                    // Clear text box and reset focus for next comment.
+                    messageInput.value = '';
+                    messageInput.focus();
+                    event.preventDefault();
+                });
+
+                // Start chat by joining queue
+                $("#chatbox_placeholder").on('click', "button[id='startChat']", function (event) {
+                    connection.invoke('joinQueue');
+                });
+
+
+
+
+
+            })
+            .catch(error => {
+                console.error(error.message);
+            });
+    });
+
+    
 });
 
