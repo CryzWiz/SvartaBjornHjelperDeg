2b0ca7c937655021c7db571f703a6e7c9ca7e63f
Benedicte Karlsen <bka049@post.uit.no>
2018-04-19 11:15:33 +0200

    restructure signalr connection

--- Bachelor_Gr4_Chatbot_MVC/wwwroot/chatbox/chat-external.js
+++ Bachelor_Gr4_Chatbot_MVC/wwwroot/chatbox/chat-external.js
@@ -28,6 +28,18 @@
     });
 })(jQuery);
 
+
+
+
+
+// Check if cookie with given cookieName exists
+function cookieExists(cookieName) {
+    if (document.cookie.indexOf(cookieName + "=") >= 0) {
+        return true;
+    }
+    return false;
+}
+
 function getSessionId() {
     //https://blogs.msmvps.com/ricardoperes/2015/10/29/persisting-signalr-connections-across-page-reloads/
     var sessionId = window.sessionStorage.sessionId;
@@ -39,40 +51,24 @@ function getSessionId() {
     return sessionId;
 }
 
+function setSignalRCookie(cookieValue) {
+    var cookieName = "SignalRCookie";
 
+    if (!cookieExists(cookieName)) {
+        var cookieValue = cookieValue;
+        var path = "/";
 
-// Starts a SignalR connection with transport fallback - if the connection cannot be started using
-// the webSockets transport the function will fallback to the serverSentEvents transport and
-// if this does not work it will try longPolling. If the connection cannot be started using
-// any of the available transports the function will return a rejected Promise.
-function startConnection(url, configureConnection) {
-    return function start(transport) {
-        console.log(`Starting connection using ${signalR.TransportType[transport]} transport`)
-        var connection = new signalR.HubConnection(url, { transport: transport });
-        if (configureConnection && typeof configureConnection === 'function') {
-            configureConnection(connection);
-        }
+        //var domain = "testDomain.com";
+        //var path = "/path";
 
-        return connection.start()
-            .then(function () {
-                return connection;
-            })
-            .catch(function (error) {
-                console.log(`Cannot start the connection use ${signalR.TransportType[transport]} transport. ${error.message}`);
-                if (transport !== signalR.TransportType.LongPolling) {
-                    return start(transport + 1);
-                }
-
-                return Promise.reject(error);
-            });
-    }(signalR.TransportType.WebSockets);
+        // Cookie expire when browser is closed
+        document.cookie = cookieName + "=" + cookieValue + ";expires=;path=" + path + ";";
+        console.log("Cookie set: " + document.cookie);
+    } else {
+        console.log("SignalRCookie exists: " + document.cookie);
+    }
 }
 
-function startConnectionWithProxy() {
-    var chatHubProxy = $.connection.chatHub;
-    //chatHubProxy.client.
-
-}
 
 function displaySentMessage(message) {
     /*var str = "<div class='chatbox__body__message chatbox__body__message--left'>";
@@ -182,7 +178,155 @@ function displayConversationEnded(startMessage) {
     displayReceivedMessage(str);
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+$(function () {
+    //document.addEventListener('DOMContentLoaded', function () {
+    var messageInput = document.getElementById('message');
+    var groupId = "";
+    var conversationId = null;
+    var chatBotToken = "";
+    var chatIsWithBot = false;
+    var conversationIdForResult = null;
+
+
+    setSignalRCookie("chat-external-cookie");
+    var connection = new signalR.HubConnection("chatHub");
+
+    /// SignalR Client methods called from hub:
+    connection.on('send', function (message, from) {
+        groupId = from;
+
+        displayReceivedMessage(message);
+        // TODO: Html encode message.
+        var encodedMsg = message;
+        // Add the sent message to the page.
+        var liElement = document.createElement('div');
+        liElement.className += "chatbox__body__message";
+        liElement.className += " chatbox__body__message--left";
+        liElement.innerHTML += '<img src="../images/narvik_kommune_small.jpg"/>';
+        liElement.innerHTML += '<p>' + encodedMsg + '</p>';
+        document.getElementById('chatbox__body').appendChild(liElement);
+        document.getElementById('chatbox__body').scrollTop = document.getElementById('chatbox__body').scrollHeight;
+        groupId = from;
+    });
+
+    connection.on("returnConnectionId", function (connectionId) {
+        console.log("returnConnectionId invoked");
+        setSignalRCookie(connectionId);
+    });
+
+    connection.on('receiveMessage', function (groupFrom, message) {
+        //groupId = groupFrom;
+        displayReceivedMessage(message);
+    });
+
+    connection.on('sendMessage', function (message) {
+        displaySentMessage(message);
+    });
+
+    connection.on('setConversationId', function (id) {
+        conversationId = id;
+    });
+
+    connection.on('setGroupId', function (id) {
+        groupId = id;
+    });
+
+    connection.on('setChatBotToken', function (token) {
+        chatBotToken = token;
+        chatIsWithBot = true;
+    });
+
+    connection.on('errorMessage', function (message) {
+        displayReceivedMessage(message);
+    });
+
+    connection.on('endBotConversation', function (message, id) {
+        resetChatBotVariables();
+        conversationIdForResult = id;
+        displayConversationEnded(message);
+    });
+
+    connection.on('conversationEnded', function (message, id) {
+        resetChatBotVariables();
+        conversationIdForResult = id;
+        groupId = "";
+        displayConversationEnded(message);
+    });
+
+    connection.on('enableInputField', function (test) {
+        messageInput.disabled = false;
+    });
+
+    // TODO: for 
+    connection.on('displayPlaceInQueue', function (queNumber) {
+        displayReceivedMessage('Du er nå lagt i kø, en medarbeider vil svare deg så raskt som mulig. Din plass i køen er: ' + queNumber);
+    });
+
+    // Start connection
+    connection.start()
+        .then(() => {
+            console.log("Connection started");
+
+            // Functions used when sending data to chathub
+            function sendMessage() {
+                if (messageInput.value.length > 0) { // if there is any input
+                    if (chatIsWithBot) {
+                        connection.invoke('sendToChatBot', conversationId, chatBotToken, messageInput.value);
+                    } else {
+                        connection.invoke('sendToGroup', groupId, messageInput.value, conversationId);
+                        //displaySentMessage(messageInput.value);
+                    }
+                    messageInput.value = '';
+                    messageInput.focus();
+                    event.preventDefault();
+                }
+            }
+
+            document.querySelector('#message').addEventListener('keypress', function (e) {
+                var key = e.which || e.keyCode;
+                if (key === 13) { // 13 is enter
+                    sendMessage();
+                }
+            });
+
+            // Send message
+            $("#sendmessage").click(function (event) {
+                sendMessage();
+            });
+
+            // Start chat by joining queue
+            $("#chatbox_placeholder").on('click', "button[id='startChat']", function (event) {
+                if (chatIsWithBot) {
+                    connection.invoke('endConversationWithChatBot', conversationId)
+                    resetChatBotVariables();
+                }
+                connection.invoke('joinQueue');
+                messageInput.disabled = true;
+            });
+
+            // Start chat-bot
+            $("#chatbox_placeholder").on('click', "button[id='startChatBot']", function (event) {
+                connection.invoke('startConversationWithChatBot');
+            });
+
+            // Store conversation result
+            $("#chatbox_placeholder").on('click', "button[id='resultYes']", function (event) {
+                connection.invoke('registerConversationResult', conversationIdForResult, true);
+                displayReceivedMessage("Takk for din tilbakemelding!");
+            });
+            $("#chatbox_placeholder").on('click', "button[id='resultNo']", function (event) {
+                connection.invoke('registerConversationResult', conversationIdForResult, false);
+                displayReceivedMessage("Takk for din tilbakemelding!");
+            });
+
+
+
+        })
+        .catch(error => { console.log(error.message); });
+
+});
+
+/*
     var messageInput = document.getElementById('message');
     var groupId = "";
     var conversationId = null;
@@ -199,6 +343,8 @@ document.addEventListener('DOMContentLoaded', function () {
         chatBotToken = "";
     }
 
+    document.cookie = "SignalRCookieTEST=TEST;expires=;path=/;";
+    setSignalRCookie("TestCookieValue");
 
     //$("#connectToSignalR").click(function () {
         // Start the connection.
@@ -220,134 +366,9 @@ document.addEventListener('DOMContentLoaded', function () {
         });*/
 
 
-        /// SignalR Client methods called from hub:
-        /*connection.on('send', function (message, from) {
-            groupId = from;
-            
-            displayReceivedMessage(message);
-            // TODO: Html encode message.
-            var encodedMsg = message;
-            // Add the sent message to the page.
-            var liElement = document.createElement('div');
-            liElement.className += "chatbox__body__message";
-            liElement.className += " chatbox__body__message--left";
-            liElement.innerHTML += '<img src="../images/narvik_kommune_small.jpg"/>';
-            liElement.innerHTML += '<p>' + encodedMsg + '</p>';
-            document.getElementById('chatbox__body').appendChild(liElement);
-            document.getElementById('chatbox__body').scrollTop = document.getElementById('chatbox__body').scrollHeight;
-            groupId = from;
-        });*/
+        
 
-        connection.on('receiveMessage', function (groupFrom, message) {
-            //groupId = groupFrom;
-            displayReceivedMessage(message);
-        });
-
-        connection.on('sendMessage', function (message) {
-            displaySentMessage(message);
-        });
-
-        connection.on('setConversationId', function (id) {
-            conversationId = id;
-        });
-
-        connection.on('setGroupId', function (id) {
-            groupId = id;
-        });
-
-        connection.on('setChatBotToken', function (token) {
-            chatBotToken = token;
-            chatIsWithBot = true;
-        });
-
-        connection.on('errorMessage', function (message) {
-            displayReceivedMessage(message);
-        });
-
-        connection.on('endBotConversation', function (message, id) {
-            resetChatBotVariables();
-            conversationIdForResult = id;
-            displayConversationEnded(message);
-        });
-
-        connection.on('conversationEnded', function (message, id) {
-            resetChatBotVariables();
-            conversationIdForResult = id;
-            groupId = "";
-            displayConversationEnded(message);
-        });
-
-        connection.on('enableInputField', function (test) {
-            messageInput.disabled = false;
-        });
-
-            // TODO: for 
-            connection.on('displayPlaceInQueue', function (queNumber) {
-                displayReceivedMessage('Du er nå lagt i kø, en medarbeider vil svare deg så raskt som mulig. Din plass i køen er: ' + queNumber);
-            });
-
-
-        })
-            .then(function (connection) {
-                console.log('connection started'); // TODO:
-
-                function sendMessage() {
-                    if (messageInput.value.length > 0) { // if there is any input
-                        if (chatIsWithBot) {
-                            connection.invoke('sendToChatBot', conversationId, chatBotToken, messageInput.value);
-                        } else {
-                            connection.invoke('sendToGroup', groupId, messageInput.value, conversationId);
-                            //displaySentMessage(messageInput.value);
-                        }
-                        messageInput.value = '';
-                        messageInput.focus();
-                        event.preventDefault();
-                    }
-                }
-
-                document.querySelector('#message').addEventListener('keypress', function (e) {
-                    var key = e.which || e.keyCode;
-                    if (key === 13) { // 13 is enter
-                        sendMessage();
-                    }
-                });
-
-                // Send message
-                $("#sendmessage").click(function (event) {
-                    sendMessage();
-                });
-                
-                // Start chat by joining queue
-                $("#chatbox_placeholder").on('click', "button[id='startChat']", function (event) {
-                    if (chatIsWithBot) {
-                        connection.invoke('endConversationWithChatBot', conversationId)
-                        resetChatBotVariables();
-                    }
-                    connection.invoke('joinQueue');
-                    messageInput.disabled = true;
-                });
-
-                // Start chat-bot
-                $("#chatbox_placeholder").on('click', "button[id='startChatBot']", function (event) {
-                    connection.invoke('startConversationWithChatBot');
-                });
-
-                // Store conversation result
-                $("#chatbox_placeholder").on('click', "button[id='resultYes']", function (event) {
-                    connection.invoke('registerConversationResult', conversationIdForResult, true);
-                    displayReceivedMessage("Takk for din tilbakemelding!");
-                });
-                $("#chatbox_placeholder").on('click', "button[id='resultNo']", function (event) {
-                    connection.invoke('registerConversationResult', conversationIdForResult, false);
-                    displayReceivedMessage("Takk for din tilbakemelding!");
-                });
-
-            })
-            .catch(error => {
-                console.error(error.message);
-            });
     //});
 
-    
-});
+
 
