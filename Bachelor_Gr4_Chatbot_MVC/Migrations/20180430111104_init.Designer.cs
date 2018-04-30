45f690389cec1570b8ac6365524491540f05bb00
Allan Arnesen <allan.arnesen@gmail.com>
2018-04-30 13:14:57 +0200

    bugfix

--- Bachelor_Gr4_Chatbot_MVC/Migrations/20180430111104_init.Designer.cs
+++ Bachelor_Gr4_Chatbot_MVC/Migrations/20180430111104_init.Designer.cs
@@ -11,8 +11,8 @@ using System;
 namespace Bachelor_Gr4_Chatbot_MVC.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20180322190012_chatbot5")]
-    partial class chatbot5
+    [Migration("20180430111104_init")]
+    partial class init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -85,6 +85,10 @@ namespace Bachelor_Gr4_Chatbot_MVC.Migrations
 
                     b.Property<string>("BotSecret");
 
+                    b.Property<int>("TypeId");
+
+                    b.Property<string>("TypeName");
+
                     b.Property<string>("baseUrl");
 
                     b.Property<string>("botAutorizeTokenScheme");
@@ -94,6 +98,8 @@ namespace Bachelor_Gr4_Chatbot_MVC.Migrations
                     b.Property<string>("contentType");
 
                     b.Property<string>("conversationUrlExtension");
+
+                    b.Property<string>("conversationUrlExtensionEnding");
 
                     b.Property<bool>("isActive");
 
@@ -108,6 +114,36 @@ namespace Bachelor_Gr4_Chatbot_MVC.Migrations
                     b.ToTable("ChatbotDetails");
                 });
 
+            modelBuilder.Entity("Bachelor_Gr4_Chatbot_MVC.Models.ChatbotTypes", b =>
+                {
+                    b.Property<int>("chatBotTypeId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int?>("ChatbotDetailschatbotId");
+
+                    b.Property<string>("Type");
+
+                    b.Property<int>("TypeId");
+
+                    b.HasKey("chatBotTypeId");
+
+                    b.HasIndex("ChatbotDetailschatbotId");
+
+                    b.ToTable("ChatbotTypes");
+                });
+
+            modelBuilder.Entity("Bachelor_Gr4_Chatbot_MVC.Models.ChatGroup", b =>
+                {
+                    b.Property<string>("ChatGroupId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("ChatGroupName");
+
+                    b.HasKey("ChatGroupId");
+
+                    b.ToTable("ChatGroup");
+                });
+
             modelBuilder.Entity("Bachelor_Gr4_Chatbot_MVC.Models.Conversation", b =>
                 {
                     b.Property<int>("ConversationId")
@@ -117,13 +153,19 @@ namespace Bachelor_Gr4_Chatbot_MVC.Migrations
 
                     b.Property<bool>("IsChatBot");
 
+                    b.Property<int>("KnowledgebaseId");
+
+                    b.Property<int?>("LinkedConversation");
+
+                    b.Property<int>("QnABaseId");
+
                     b.Property<bool>("Result");
 
                     b.Property<DateTime>("StartTime");
 
-                    b.Property<int>("UserGroup1");
+                    b.Property<string>("UserGroup1");
 
-                    b.Property<int>("UserGroup2");
+                    b.Property<string>("UserGroup2");
 
                     b.HasKey("ConversationId");
 
@@ -174,6 +216,115 @@ namespace Bachelor_Gr4_Chatbot_MVC.Migrations
                     b.HasKey("OpeningHoursId");
 
                     b.ToTable("OpeningHours");
+                });
+
+            modelBuilder.Entity("Bachelor_Gr4_Chatbot_MVC.Models.QnAKeywordPair", b =>
+                {
+                    b.Property<int>("QnAKeywordPairId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Answer");
+
+                    b.Property<string>("Query");
+
+                    b.HasKey("QnAKeywordPairId");
+
+                    b.ToTable("QnAKeywordPairs");
+                });
+
+            modelBuilder.Entity("Bachelor_Gr4_Chatbot_MVC.Models.QnAViewModels.QnABaseClass", b =>
+                {
+                    b.Property<int>("QnAId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("chatbotName");
+
+                    b.Property<bool>("isActive");
+
+                    b.Property<string>("knowledgeBaseID");
+
+                    b.Property<DateTime>("lastEdit");
+
+                    b.Property<DateTime>("regDate");
+
+                    b.Property<string>("subscriptionKey");
+
+                    b.HasKey("QnAId");
+
+                    b.ToTable("QnABaseClass");
+                });
+
+            modelBuilder.Entity("Bachelor_Gr4_Chatbot_MVC.Models.QnAViewModels.QnAKnowledgeBase", b =>
+                {
+                    b.Property<int>("QnAKnowledgeBaseId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("ConversationCount");
+
+                    b.Property<bool>("IsActive");
+
+                    b.Property<string>("KnowledgeBaseID");
+
+                    b.Property<DateTime>("LastEdit");
+
+                    b.Property<int>("QnABotId");
+
+                    b.Property<string>("QnAKnowledgeName");
+
+                    b.Property<DateTime>("RegDate");
+
+                    b.HasKey("QnAKnowledgeBaseId");
+
+                    b.ToTable("QnAKnowledgeBase");
+                });
+
+            modelBuilder.Entity("Bachelor_Gr4_Chatbot_MVC.Models.QnAViewModels.QnAPairs", b =>
+                {
+                    b.Property<int>("QnAPairsId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Answer");
+
+                    b.Property<string>("Dep");
+
+                    b.Property<int>("KnowledgeBaseId");
+
+                    b.Property<bool>("Published");
+
+                    b.Property<DateTime>("PublishedDate");
+
+                    b.Property<string>("PublishingType");
+
+                    b.Property<int?>("QnAKnowledgeBaseId");
+
+                    b.Property<string>("Query");
+
+                    b.Property<bool>("Trained");
+
+                    b.Property<DateTime>("TrainedDate");
+
+                    b.HasKey("QnAPairsId");
+
+                    b.HasIndex("QnAKnowledgeBaseId");
+
+                    b.ToTable("QnAPairs");
+                });
+
+            modelBuilder.Entity("Bachelor_Gr4_Chatbot_MVC.Models.UserChatGroup", b =>
+                {
+                    b.Property<string>("UserId");
+
+                    b.Property<string>("ChatGroupId");
+
+                    b.Property<string>("ApplicationUserId");
+
+                    b.HasKey("UserId", "ChatGroupId");
+
+                    b.HasIndex("ApplicationUserId");
+
+                    b.HasIndex("ChatGroupId");
+
+                    b.ToTable("UserChatGroup");
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRole", b =>
@@ -284,11 +435,37 @@ namespace Bachelor_Gr4_Chatbot_MVC.Migrations
                     b.ToTable("AspNetUserTokens");
                 });
 
+            modelBuilder.Entity("Bachelor_Gr4_Chatbot_MVC.Models.ChatbotTypes", b =>
+                {
+                    b.HasOne("Bachelor_Gr4_Chatbot_MVC.Models.ChatbotDetails")
+                        .WithMany("chatbotTypes")
+                        .HasForeignKey("ChatbotDetailschatbotId");
+                });
+
             modelBuilder.Entity("Bachelor_Gr4_Chatbot_MVC.Models.Message", b =>
                 {
                     b.HasOne("Bachelor_Gr4_Chatbot_MVC.Models.Conversation", "Conversation")
                         .WithMany("Messages")
                         .HasForeignKey("ConversationId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("Bachelor_Gr4_Chatbot_MVC.Models.QnAViewModels.QnAPairs", b =>
+                {
+                    b.HasOne("Bachelor_Gr4_Chatbot_MVC.Models.QnAViewModels.QnAKnowledgeBase")
+                        .WithMany("QnAPairs")
+                        .HasForeignKey("QnAKnowledgeBaseId");
+                });
+
+            modelBuilder.Entity("Bachelor_Gr4_Chatbot_MVC.Models.UserChatGroup", b =>
+                {
+                    b.HasOne("Bachelor_Gr4_Chatbot_MVC.Models.ApplicationUser", "ApplicationUser")
+                        .WithMany("ChatGroups")
+                        .HasForeignKey("ApplicationUserId");
+
+                    b.HasOne("Bachelor_Gr4_Chatbot_MVC.Models.ChatGroup", "ChatGroup")
+                        .WithMany("GroupMembers")
+                        .HasForeignKey("ChatGroupId")
                         .OnDelete(DeleteBehavior.Cascade);
                 });
 
